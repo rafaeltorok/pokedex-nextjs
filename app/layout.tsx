a79f25844136c34b947f8e7be16c874d0bd89dd2
rafaeltorok <rafaeltorok@gmail.com>
2026-09-03 13:48:41 -0300

    update: add metadata for the page, including a title and description - re-organize the order for the imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
-import localFont from "next/font/local";
 import "./globals.css";
 
+// Components
 import NavBar from "@/components/NavBar";
+
+// TypeScript types
+import type { Metadata } from "next";
+
+// Custom font
+import localFont from "next/font/local";
 
 const pokeFont = localFont({
   src: "./fonts/pokemon.ttf",
 });
+
+export const metadata: Metadata = {
+  title: "Pokédex App",
+  description: "A Pokédex-style app built with Next.js",
+}
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
