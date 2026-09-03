de5ecde378277fd216d1a484ca19de3663e248c8
rafaeltorok <rafaeltorok@gmail.com>
2026-09-03 13:47:49 -0300

    fix: add config option for local devices to access the Next server

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
       new URL("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/*.png"),
     ],
   },
+  // Allows all local devices access to the Next Dev Server
+  allowedDevOrigins: ["192.168.0.*:3000", "192.168.0.*", "0.0.0.0", "0.0.0.0:3000"],
 };
 
 export default nextConfig;
