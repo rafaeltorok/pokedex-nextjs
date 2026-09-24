1ed81be1f8c9f5e30211040ac5d484c626269870
Rafael Torok <rafaeltorok@gmail.com>
2026-09-24 11:03:14 -0300

    refactor: move all navigation logic to the parent component of NavArrows

--- components/PokemonPage/NavArrows.tsx
+++ components/PokemonPage/NavArrows.tsx
@@ -1,44 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-// TypeScript types
-import type { PokemonApiResource } from "@/types/types";
-
 interface NavArrowsProps {
-  pokemonList: PokemonApiResource[];
-  pokemonName: string;
+  previous: string;
+  next: string;
   regionName: string;
 }
 
-export default function NavArrows({ pokemonList, pokemonName, regionName }: NavArrowsProps) {
-  const router = useRouter();
-
-  // Define the previous and next pages based on the current Pokédex entry position
-  const currentPokemonIndex = pokemonList.findIndex(poke => poke.name === pokemonName);
-  const previous = pokemonList[currentPokemonIndex - 1]?.name || null;
-  const next = pokemonList[currentPokemonIndex + 1]?.name || null;
-
-  // Handles keyboard navigation
-  useEffect(() => {
-    function onKeyDown(event: KeyboardEvent) {
-      // Left arrow key
-      if (event.key === "ArrowLeft" && previous) {
-        router.push(`/${regionName}/${previous}`);
-      }
-
-      // Right arrow key
-      if (event.key === "ArrowRight" && next) {
-        router.push(`/${regionName}/${next}`);
-      }
-    }
-
-    window.addEventListener("keydown", onKeyDown);
-    return () => window.removeEventListener("keydown", onKeyDown);
-  }, [previous, next, router, regionName]);
-
+export default function NavArrows({ previous, next, regionName }: NavArrowsProps) {
   return (
     <div>
       {/* Previous arrow */}
@@ -46,7 +16,7 @@ export default function NavArrows({ pokemonList, pokemonName, regionName }: NavA
         <div
           className="
             absolute
-            -left-10
+            -left-9
             text-4xl
             p-1
             border-1 border-gray-700 rounded
@@ -63,7 +33,7 @@ export default function NavArrows({ pokemonList, pokemonName, regionName }: NavA
         <div
           className="
             absolute
-            -right-10
+            -right-9
             text-4xl
             p-1
             border-1 border-gray-700 rounded
